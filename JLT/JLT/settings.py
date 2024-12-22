d9a155d4c06fe2595283dd9615adc1fbd4c50d1d
maxime garcia <maxime-garcia@windowslive.com>
2024-12-22 14:00:40 -0500

    improves

--- JLT/JLT/settings.py
+++ JLT/JLT/settings.py
@@ -97,10 +97,18 @@ WSGI_APPLICATION = 'JLT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'maxoufaya33$JLT',  # Replace with your database name
+        'USER': 'maxoufaya33',       # Replace with your MySQL username
+        'PASSWORD': 'Udk7ke25#',   # Replace with your MySQL password
+        'HOST': 'localhost',           # Or the IP address of your MySQL server
+        'PORT': '3306',                # Default MySQL port
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',  # Recommended for MySQL
+        },
     }
 }
+
 
 
 
